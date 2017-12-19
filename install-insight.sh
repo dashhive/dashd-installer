e594153f5f352705dd67057caa45f39b532313eb
AJ ONeal <coolaj86@gmail.com>
2017-12-19 05:10:11 +0000

    update insight installer

--- install-insight.sh
+++ install-insight.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 dash_prefix=/opt/dashcore
-my_npm=$dash_prefix/node/bin/npm
+my_npm="$dash_prefix/node/bin/node $dash_prefix/node/bin/npm"
 mkdir -p $dash_prefix
 
 sudo apt install -y wget curl git python
 export NODE_PATH=$dash_prefix/node/lib/node_modules
 export NODE_VERSION=v8.9.3
+export PATH=$dash_prefix/node/bin:$PATH
 echo $NODE_VERSION > /tmp/NODEJS_VER
 curl -fsSL bit.ly/node-installer | bash -s -- --no-dev-deps
 
@@ -19,5 +20,5 @@ pushd $dash_prefix/bitcore
   $my_npm install insight-ui-dash --S
 
   chmod a+x ./bin/bitcore-node-dash
-  $dash_prefix/node/bin/node $dash_prefix/bitcore/bin/bitcore-node-dash
+  $dash_prefix/node/bin/node $dash_prefix/bitcore/bin/bitcore-node-dash start -c $dash_prefix/
 popd
